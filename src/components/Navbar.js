765378be0fe23eac77839f32f7dbff78355551e5
Sergey Yurin <sergey.yurin@yahoo.com>
2021-05-24 23:27:11 -0500

    commit to 'main'

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../cover.png';
+import logo from '../logo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-scroll';
